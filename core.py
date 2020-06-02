4cd6f076af464df28e311e25d6fab27dd86f21cb
Yennda <bukacek@ufe.cz>
2020-06-02 10:19:55 +0200

    second commit

--- core.py
+++ core.py
@@ -2,40 +2,73 @@ import numpy as np
 
 import tools as tl
 
+
 class Core(object):
     def __init__(self, folder, file):
         self.folder = folder
         self.file = file
 
-        self._video = None
+        self._raw = None
         self._time_info = None
 
-    def load_data(self):
-        self.__video_stats = self.load_stats()
-        self._video = self.load_video()
+        self.__video_stats = None
 
+        self.k = 1
 
-    def load_stats(self):
+        self._load_data()
+
+    def _load_data(self):
+        self.__video_stats = self._load_stats()
+        self._raw = self._load_video()
+
+    def _load_stats(self):
         suffix = '.tsv'
         with open(self.folder + self.file + suffix, mode='r') as fid:
             file_content = fid.readlines()
 
-        self.time_info = tl.frame_times(file_content)
+        self._time_info = tl.frame_times(file_content)
         stats = file_content[1].split()
         video_length = len(file_content) - 1
         video_width = int(stats[1])
-        video_hight = int(stats[2])
-        video_fps = float(stats[4]) * int(stats[5])
+        video_height = int(stats[2])
 
-        return [video_fps, [video_width, video_hight, video_length]]
+        return video_width, video_height, video_length
 
-    def load_video(self):
+    def _load_video(self):
         with open(self.folder + self.file + '.bin', mode='rb') as fid:
             video = np.fromfile(fid, dtype=np.float64)
             fid.close()
 
-        video = np.reshape(video, (self.__video_stats[1][0],
-                                   self.__video_stats[1][1],
-                                   self.__video_stats[1][2]), order='F')
+        video = np.reshape(video, (self.__video_stats[0],
+                                   self.__video_stats[1],
+                                   self.__video_stats[2]), order='F')
 
         return np.swapaxes(video, 0, 1)
+
+    def _load_spr(self):
+        self.spr_signals = []
+
+        for c in self._channels:
+            f = open(self.folder + NAME_GLOBAL_SPR + '_{}.tsv'.format(c + 1), 'r')
+            contents = f.readlines()
+
+            time = []
+            signal = []
+
+            for line in contents[:-1]:
+                line_split = line.split('\t')
+
+                if c == 0:
+                    time.append(float(line_split[0]))
+                signal.append(float(line_split[1]))
+
+            if c == 0:
+                self.spr_time = time
+            self.spr_signals.append(signal)
+
+    def __len__(self):
+        return self._raw.shape[2]
+
+    @property
+    def shape(self):
+        return self._raw.shape
--- main.py
+++ main.py
@@ -1,13 +1,10 @@
 from core import Core
 
 main_folder = 'C:/SPRUP_data_Jenda/2019_03_13_Jenda_microscopy/'
-folder = main_folder + '20_04_21_L3_tomas/'
-file = 'raw_01_4'
+folder = main_folder + '20_05_26_K5/'
+file = 'raw_01_2'
 
 core = Core(folder, file)
 
-print(core.file)
 
-print(core.load_stats())
-core.load_data()
-print(core._video.shape)
+print(core.shape)
